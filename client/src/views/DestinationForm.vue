696f713c5baf09b6ca03d0a8354790e3f2e9787e
Davis Odom <deavisdude@gmail.com>
2023-03-25 14:14:34 -0400

    remove whitespace

--- client/src/views/DestinationForm.vue
+++ client/src/views/DestinationForm.vue
@@ -61,7 +61,6 @@ export default {
         }
       }
     }
-    
   },
   methods: {
     async handleSubmit() {
@@ -78,8 +77,6 @@ export default {
           });
           this.$emit('close');
         }
-        
-    
       } catch (error) {
         console.error(error)
       }
